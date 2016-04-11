4685fc3ff1d2897e3626c48611340d264960a18f
Wojtek Czekalski <me@wczekalski.com>
2016-04-11 18:34:50 +0200

    Made Queue.swift internal

--- Graph/Queue.swift
+++ Graph/Queue.swift
@@ -1,22 +1,22 @@
 
-public struct Queue<T> {
+internal struct Queue<T> {
     private var elements: [T] = []
     
-    public mutating func enqueue(element: T) {
+    mutating func enqueue(element: T) {
         elements.append(element)
     }
     
-    public mutating func enqueue(elements: [T]) {
+    mutating func enqueue(elements: [T]) {
         elements.forEach {self.elements.append($0)}
     }
     
-    public mutating func dequeue() -> T {
+    mutating func dequeue() -> T {
         return elements.removeFirst()
     }
     
-    public func isEmpty() -> Bool {
+    func isEmpty() -> Bool {
         return elements.count == 0
     }
     
-    public init() {}
+    init() {}
 }
