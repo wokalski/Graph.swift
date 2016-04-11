8f4d5a778812f4cf5ae6a4c5a88e6f1e7a748c78
Wojtek Czekalski <me@wczekalski.com>
2016-04-11 13:44:16 +0200

    Added documentation for all public functions

--- Graph/Graph.swift
+++ Graph/Graph.swift
@@ -1,7 +1,13 @@
-
+/**
+ Node is a building block of any `Graph`.
+*/
 public protocol Node: Hashable {
     var children: [Self] { get }
 }
+
+/**
+ Graph defines basic directed graph. Instances of conforming can leverage basic Graph algorithms defined in the extension.
+ */
 
 public protocol Graph {
     associatedtype T : Node
@@ -11,48 +17,65 @@ public protocol Graph {
 public extension Graph {
     
     /**
-     Returns true if a graph doesn't contain a cycle.
+     Checks whether a graph contains a cycle. A cycle means that there is a parent-child relation where child is also a predecessor of the parent.
      
-     A cycle means that there is a parent-child relation where child is also a predecessor of the parent.
+     - Complexity: O(N + E) where N is the number of all nodes, and E is the number of connections between them
+     - returns: `true` if doesn't contain a cycle
      */
     func isAcyclic() -> Bool {
-        var result = true
+        var isAcyclic = true
         
-        depthSearch(SearchHandler(newEdgeFound: SearchHandler.isAcyclicHandler(&result)))
-        return result
+        depthSearch(edgeFound: { edge in
+            isAcyclic = (edge.type() != .Back)
+            return isAcyclic
+            }, nodeStatusChanged: nil)
+        
+        return isAcyclic
     }
     
     /**
-     Topological sort is an ordering of graph's nodes such that for every directed edge u->v, u comes before v in the ordering.
+     Topological sort ([Wikipedia link](https://en.wikipedia.org/wiki/Topological_sorting)) is an ordering of graph's nodes such that for every directed edge u->v, u comes before v in the ordering.
      
+     - Complexity: O(N + E) where N is the number of all nodes, and E is the number of connections between them
      - returns: Ordered array of nodes or nil if it cannot be done (i.e. the graph contains cycles)
      */
     func topologicalSort() -> [T]? {
         var orderedVertices = [T]()
         var isAcyclic = true
-        
-        depthSearch(SearchHandler<T>(nodeChangedStatus: { nodeInfo in
+        let nodeProcessed = { (nodeInfo: NodeInfo<T>) -> Bool in
             if nodeInfo.status == .Processed {
                 orderedVertices.append(nodeInfo.node)
             }
             return true
-            }, newEdgeFound: SearchHandler.isAcyclicHandler(&isAcyclic)))
+        }
+        
+        depthSearch(edgeFound: { edge in
+                isAcyclic = (edge.type() != .Back)
+                return isAcyclic
+            }
+            , nodeStatusChanged: nodeProcessed)
         
         return isAcyclic ? orderedVertices : nil
     }
     
     /**
-     Breadth First Search
+     Breadth First Search ([Wikipedia link](https://en.wikipedia.org/wiki/Breadth-first_search))
+     
+     - Complexity: O(N + E) where N is the number of all nodes, and E is the number of connections between them
+     - Parameter edgeFound: Called whenever a new connection between nodes is discovered
+     - Parameter nodeStatusChanged: Called when a node changes its status. It is called twice for every node
+     - Note: Search will stop if `false` is returned from any closure.
+     - Warning: It will only call `nodeStatusChanged:` on tree edges because we don't maintain entry and exit times of nodes. More information about entry and exit times [here](https://courses.csail.mit.edu/6.006/fall11/rec/rec14.pdf). It is very easy to implement them, but might have big influence on memory use.
      */
-    func breadthSearch(handler: SearchHandler<T>?) {
+    
+    func breadthSearch(edgeFound edgeFound: (Edge<T> -> Bool)?, nodeStatusChanged: (NodeInfo<T> -> Bool)?) {
         var queue = Queue<T>()
         var searchInfo = SearchInfo<T>()
         
-        for node in nodes {
-             if searchInfo.status(node) == .New {
-                queue.enqueue(node)
-                searchInfo.set(node, status: .Discovered)
-                handler?.nodeChangedStatus?(searchInfo.nodeInfo(node))
+        for graphRoot in nodes {
+             if searchInfo.status(graphRoot) == .New {
+                queue.enqueue(graphRoot)
+                if updateNodeStatus(&searchInfo, node: graphRoot, status: .Discovered, nodeStatusChanged: nodeStatusChanged) == false { return }
                 
                 while queue.isEmpty() == false {
                     let parent = queue.dequeue()
@@ -60,43 +83,59 @@ public extension Graph {
                     for child in parent.children {
                         if searchInfo.status(child) == .New {
                             queue.enqueue(child)
-                            searchInfo.set(child, status: .Discovered)
-                            handler?.newEdgeFound?(Edge(from: searchInfo.nodeInfo(parent), to: searchInfo.nodeInfo(child)))
+                            if updateNodeStatus(&searchInfo, node: child, status: .Discovered, nodeStatusChanged: nodeStatusChanged) == false { return }
+                            if let c = edgeFound?(Edge(from: searchInfo.nodeInfo(parent), to: searchInfo.nodeInfo(child))) where c == false { return } // Since we don't maintain entry and exit times, we can only rely on tree edges type.
                         }
                     }
-                    searchInfo.set(parent, status: .Processed)
-                    handler?.nodeChangedStatus?(searchInfo.nodeInfo(parent))
+                    if updateNodeStatus(&searchInfo, node: parent, status: .Processed, nodeStatusChanged: nodeStatusChanged) == false { return }
                 }
             }
         }
     }
     
     /**
-     Depth First Search
+     Depth First Search ([Wikipedia link](https://en.wikipedia.org/wiki/Depth-first_search))
+     
+     - Complexity: O(N + E) where N is the number of all nodes, and E is the number of connections between them
+     - Parameter edgeFound: Called whenever a new connection between nodes is discovered
+     - Parameter nodeStatusChanged: Called when a node changes its status. It is called twice for every node
+     - Note: Search will stop if `false` is returned from any closure.
+     - Warning: This function makes recursive calls. Keep it in mind when operating on big data sets.
      */
-    func depthSearch(handler: SearchHandler<T>?) {
+    func depthSearch(edgeFound edgeFound: (Edge<T> -> Bool)?, nodeStatusChanged: (NodeInfo<T> -> Bool)?) {
         var searchInfo = SearchInfo<T>()
         
         for node in nodes {
             if searchInfo.status(node) == .New {
-                dfsVisit(node, searchInfo: &searchInfo, handler: handler)
+                if dfsVisit(node, searchInfo: &searchInfo, edgeFound: edgeFound, nodeStatusChanged: nodeStatusChanged) == false {
+                    return
+                }
             }
         }
     }
     
-    private func dfsVisit(node: T, inout searchInfo: SearchInfo<T>, handler: SearchHandler<T>?) {
+    private func updateNodeStatus(inout searchInfo: SearchInfo<T>, node: T, status: NodeStatus, nodeStatusChanged: (NodeInfo<T> -> Bool)?) -> Bool {
+        searchInfo.set(node, status: status)
+        guard let shouldContinue = nodeStatusChanged?(searchInfo.nodeInfo(node)) else {
+            return true
+        }
+        return shouldContinue
+    }
+
+    private func dfsVisit(node: T, inout searchInfo: SearchInfo<T>, edgeFound: (Edge<T> -> Bool)?, nodeStatusChanged: (NodeInfo<T> -> Bool)?) -> Bool {
         
-        searchInfo.set(node, status: .Discovered)
-        handler?.nodeChangedStatus?(NodeInfo(node: node, status: .Discovered))
+        if updateNodeStatus(&searchInfo, node: node, status: .Discovered, nodeStatusChanged: nodeStatusChanged) == false { return false }
         
         for child in node.children {
-            handler?.newEdgeFound?(Edge(from: searchInfo.nodeInfo(node), to: searchInfo.nodeInfo(child)))
+            if let c = edgeFound?(Edge(from: searchInfo.nodeInfo(node), to: searchInfo.nodeInfo(child))) where c == false { return false }
             if searchInfo.status(child) == .New {
-                dfsVisit(child, searchInfo: &searchInfo, handler: handler)
+                if dfsVisit(child, searchInfo: &searchInfo, edgeFound: edgeFound, nodeStatusChanged: nodeStatusChanged) == false {
+                    return false
+                }
             }
         }
         
-        searchInfo.nodeStatus[node] = .Processed
-        handler?.nodeChangedStatus?(NodeInfo(node: node, status: .Processed))
+        if updateNodeStatus(&searchInfo, node: node, status: .Processed, nodeStatusChanged: nodeStatusChanged) == false { return false }
+        return true
     }
 }
--- Graph/SearchStateUtils.swift
+++ Graph/SearchStateUtils.swift
@@ -6,23 +6,44 @@
 //  Copyright © 2016 wczekalski. All rights reserved.
 //
 
+/**
+ Defines all possible states for a node in a graph.
+ */
 public enum NodeStatus {
+    ///A node which has not been discovered yet
     case New
+    ///The node has been discovered. Its children have not been discovered yet.
     case Discovered
+    /**
+     The node has been processed. Its children were discovered.
+        - In DFS it implies that all descendants are Processed too since it goes depth first.
+     */
     case Processed
 }
 
+/**
+ A container for `Node` and its `NodeStatus`
+ */
 public struct NodeInfo<T: Node> {
     let node: T
     let status: NodeStatus
 }
 
+/**
+ Defines edge types
+*/
 public enum EdgeType {
-    case CrossOrForward
-    case Back
+    ///An edge to a node which has not been discovered yet.
     case Tree
+    ///An edge to a node which is a predecessor of edge's source node. Implies that a graph is not acyclic.
+    case Back
+    ///An edge to a node which is not a direct descendant of a node or to a sybling graph.
+    case CrossOrForward
 }
 
+/**
+ Describes a relation between nodes.
+*/
 public struct Edge<T: Node> {
     let from: NodeInfo<T>
     let to: NodeInfo<T>
@@ -38,21 +59,15 @@ public struct Edge<T: Node> {
     }
 }
 
-public struct SearchHandler<T: Node> {
-    let nodeChangedStatus: (NodeInfo<T> -> Bool)?
-    let newEdgeFound: (Edge<T> -> Bool)?
-    
-    init(nodeChangedStatus: (NodeInfo<T> -> Bool)? = nil, newEdgeFound: (Edge<T> -> Bool)? = nil) {
-        self.nodeChangedStatus = nodeChangedStatus
-        self.newEdgeFound = newEdgeFound
-    }
-}
-
 struct SearchInfo<T: Node> {
     var nodeStatus = Dictionary<T, NodeStatus>()
     
     mutating func set(node: T, status: NodeStatus) {
-        nodeStatus[node] = status
+        if nodeStatus[node] != status {
+            nodeStatus[node] = status
+        } else {
+            abort()
+        }
     }
     
     func status(node: T) -> NodeStatus {
@@ -61,17 +76,5 @@ struct SearchInfo<T: Node> {
     
     func nodeInfo(node: T) -> NodeInfo<T> {
         return NodeInfo(node: node, status: status(node))
-    }
-}
-
-extension SearchHandler {
-    static func isAcyclicHandler(inout result: Bool) -> (Edge<T> -> Bool)? {
-        return { edge in
-            if edge.to.status == .Discovered {
-                result = false
-                return false
-            }
-            return true
-        }
     }
 }
